507b15f4b4ecd764a18238f34dfcf0221dce9e30
mukti patel <mukti22patel@gmail.com>
2026-03-20 17:58:26 +0530

    Added Db Context

--- WebApplication4/WebApplication4/Program.cs
+++ WebApplication4/WebApplication4/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
